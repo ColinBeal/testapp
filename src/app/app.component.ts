b5d508c8c4fb0d0c47030496514a9c9004ef27bd
ColinBeal <colinbealwork@gmail.com>
2022-04-05 00:54:23 +0200

    Add pdf sent message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,94 +1,89 @@
-import { Component }    from '@angular/core';
-import { HttpClient }   from '@angular/common/http'
-import { HttpHeaders }  from '@angular/common/http';
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    'Authorization': 'Bearer b0084ba396b1821256dbe44de774889d',
-    'Content-Type':  'application/json',
+    Authorization: 'Bearer b0084ba396b1821256dbe44de774889d',
+    'Content-Type': 'application/json',
   }),
 };
 
 const defaultMembers = [
   {
-    "firstname": "John",
-    "lastname": "Doe",
-    "email": "john.doe@yousign.fr",
-    "phone": "+33612345678",
-    "fileObjects": [
+    firstname: 'John',
+    lastname: 'Doe',
+    email: 'john.doe@yousign.fr',
+    phone: '+33612345678',
+    fileObjects: [
       {
-        "file": "/files/", //dummy fileId
-        "page": 1,
-        "position": "230,499,464,589",
-        "mention": "Read and approved",
-        "mention2": "Signed by John Doe"
-      }
-    ]
+        file: '/files/', //dummy fileId
+        page: 1,
+        position: '230,499,464,589',
+        mention: 'Read and approved',
+        mention2: 'Signed by John Doe',
+      },
+    ],
   },
   {
-    "firstname": "Colin",
-    "lastname": "BEAL",
-    "email": "colinbealwork@gmail.com",
-    "phone": "+33786861417",
-    "fileObjects": [
+    firstname: 'Colin',
+    lastname: 'BEAL',
+    email: 'colinbealwork@gmail.com',
+    phone: '+33786861417',
+    fileObjects: [
       {
-        "file": "/files/",
-        "page": 1,
-        "position": "130,199,164,489",
-        "mention": "Read and approved",
-        "mention2": "Signed by Colin BEAL"
-      }
-    ]
-  }
-]
+        file: '/files/',
+        page: 1,
+        position: '130,199,164,489',
+        mention: 'Read and approved',
+        mention2: 'Signed by Colin BEAL',
+      },
+    ],
+  },
+];
 
 interface IMemberJson {
-  firstname: string,
-  lastname:  string,
-  email:     string,
-  phone:     string,
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
   fileObjects: Array<{
-    file: string,
-    page: number,
-    position: string,
-    mention:  string,
-    mention2: string
-  }>
+    file: string;
+    page: number;
+    position: string;
+    mention: string;
+    mention2: string;
+  }>;
 }
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-
 export class AppComponent {
   title = 'test-edusign';
 
-  private httpOptions    = httpOptions;
+  private httpOptions = httpOptions;
   private defaultMembers = defaultMembers;
 
-  public isReady: boolean = false;
+  public pdfSent: boolean = false;
   public base64?: string;
   public members: Array<IMemberJson> = [];
-  public file?:   {name: string, content: any};
+  public file?: { name: string; content: any };
   public fileId?: string;
 
-
-  constructor(
-    private httpClient: HttpClient
-  ) {}
+  constructor(private httpClient: HttpClient) {}
 
   onFileChange(event: Event) {
     const target = event.target as HTMLInputElement;
     const file: File = (target.files as FileList)[0];
     if (!!file) {
-      this.getBase64(file)
-      .then((data: any) => {
+      this.getBase64(file).then((data: any) => {
         this.file = {
           name: file.name,
-          content: data
-        }
+          content: data,
+        };
       });
     }
   }
@@ -98,16 +93,22 @@ export class AppComponent {
       const reader = new FileReader();
       reader.readAsDataURL(file);
       //only returns the pdf content
-      reader.onload = () => resolve((<any> reader.result).split(',')[1]);
-      reader.onerror = error => reject(error);
+      reader.onload = () => resolve((<any>reader.result).split(',')[1]);
+      reader.onerror = (error) => reject(error);
     });
   }
 
   onFileSubmit() {
-    this.httpClient.post('https://staging-api.yousign.com/files', this.file, this.httpOptions).subscribe(res => {
-      this.members = this.defaultMembers;
-      this.fileId = (<any>res).id
-    })
+    this.httpClient
+      .post(
+        'https://staging-api.yousign.com/files',
+        this.file,
+        this.httpOptions
+      )
+      .subscribe((res) => {
+        this.members = this.defaultMembers;
+        this.fileId = (<any>res).id;
+      });
   }
 
   removeMember(index: number) {
@@ -118,16 +119,27 @@ export class AppComponent {
     for (let i = 0; i < this.members.length; i++) {
       //replace the dummy filepath and mention with the correct ones
       this.members[i].fileObjects[0].file = fileId;
-      this.members[i].fileObjects[0].mention2 = "Signed by " + this.members[i].firstname + ' ' + this.members[i].lastname
+      this.members[i].fileObjects[0].mention2 =
+        'Signed by ' +
+        this.members[i].firstname +
+        ' ' +
+        this.members[i].lastname;
     }
     let data = {
-      name: "My first procedure",
-      description: "Awesome! Here is the description of my first procedure",
-      members: this.members
-    }
+      name: 'My first procedure',
+      description: 'Awesome! Here is the description of my first procedure',
+      members: this.members,
+    };
 
-    this.httpClient.post('https://staging-api.yousign.com/procedures', data, this.httpOptions).subscribe(res => {
-      console.log("Document envoyé !", res) //so we know it works :)
-    })
+    this.httpClient
+      .post(
+        'https://staging-api.yousign.com/procedures',
+        data,
+        this.httpOptions
+      )
+      .subscribe((res) => {
+        console.log('Document envoyé !', res); //so we know it works :)
+        this.pdfSent = true;
+      });
   }
 }
